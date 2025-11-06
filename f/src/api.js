c0884eb3e31da19561fc2e0ca5f0fe69a87b2dd7
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-11-06 21:45:44 +0300

    Update api.js

--- f/src/api.js
+++ f/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://chatpdf-production.up.railway.app";
 
 export const uploadPDFs = (files) => {
   const formData = new FormData();
