6576e334c5e11151bf149dcba9b06dafafe4add8
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-11-06 19:50:50 +0300

    Update services.py

--- b/services.py
+++ b/services.py
@@ -1,5 +1,5 @@
 import os, shutil, uuid
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_cohere import CohereEmbeddings # ADDED: For free, remote embeddings
 from langchain_community.document_loaders import PyMuPDFLoader 
 from langchain_community.vectorstores import Chroma
